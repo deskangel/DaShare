04c2f7320ffb8f82ec394335b6c781817897f920
WilliamX <williamx@deskangel.com>
2021-03-05 22:19:27 +0800

    * update

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -2,9 +2,13 @@
 //  Generated file. Do not edit.
 //
 
+// clang-format off
+
 import FlutterMacOS
 import Foundation
 
+import package_info
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTPackageInfoPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlugin"))
 }
