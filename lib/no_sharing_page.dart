07ee27d10444496069d2f9703b18be4c506e3634
WilliamX <williamx@deskangel.com>
2023-08-29 21:50:32 +0800

    + mobile_scanner

--- lib/no_sharing_page.dart
+++ lib/no_sharing_page.dart
@@ -1,19 +1,39 @@
-import 'package:flutter/material.dart';
+import 'dart:typed_data';
 
-class NoSharingPage extends StatelessWidget {
-  const NoSharingPage(this.appTitle,  this.appBarActions, {super.key});
+import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+class NoSharingPage extends StatefulWidget {
+  const NoSharingPage(this.appTitle, this.appBarActions, {super.key});
 
   final Widget appTitle;
   final List<Widget> appBarActions;
 
   @override
+  State<NoSharingPage> createState() => _NoSharingPageState();
+}
+
+class _NoSharingPageState extends State<NoSharingPage> {
+  bool scan = false;
+
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: this.appTitle,
-        actions: this.appBarActions,
+        title: this.widget.appTitle,
+        actions: this.widget.appBarActions,
       ),
-      body: Column(
+      body: scan? MobileScanner(
+        // fit: BoxFit.contain,
+        onDetect: (capture) {
+          final List<Barcode> barcodes = capture.barcodes;
+          final Uint8List? image = capture.image;
+          for (final barcode in barcodes) {
+            debugPrint('Barcode found! ${barcode.rawValue}');
+          }
+        },
+      ):  Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: const [
           Text(
@@ -30,6 +50,21 @@ class NoSharingPage extends StatelessWidget {
           ),
         ],
       ),
+      persistentFooterButtons: buildPersistentButtons(),
     );
+  }
+
+  List<Widget> buildPersistentButtons() {
+    return [
+      TextButton.icon(
+        icon: const Icon(Icons.queue_play_next),
+        label: const Text('Scane'),
+        onPressed: () async {
+          setState(() {
+            scan = true;
+          });
+        },
+      ),
+    ];
   }
 }
