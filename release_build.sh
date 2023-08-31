48a798b21fbfe13e2fef724711a44c3499704620
WilliamX <williamx@deskangel.com>
2023-08-31 15:38:28 +0800

    * improved build script

--- release_build.sh
+++ release_build.sh
@@ -3,25 +3,74 @@
 BUILD_NUMBER=`cat .build_number`
 BUILD_NUMBER=$[BUILD_NUMBER+1]
 
-if [[ "$1" == "-h" ]]; then
-    echo "usage: $0 [apk | app | --deploy] [--deploy]"
-    exit 0
-elif [[ "$1" == "app" ]]; then
+YEAR_NUMBER=`date +"%Y"`
+
+###################################################################
+
+OPT_APP_FORMAT=""
+OPT_DEPLOYMENT=""
+OPT_UPLOAD=""
+BUILD_RESULT=0
+
+for optname in "$@"
+do
+    case $optname in
+        app)
+            OPT_APP_FORMAT="app"
+            ;;
+        apk)
+            OPT_APP_FORMAT="apk"
+            ;;
+        -d)
+            OPT_DEPLOYMENT="-d"
+            ;;
+        -u)
+            OPT_UPLOAD="-u"
+            ;;
+        -h)
+            echo "usage: $0 [apk | app] [-d]"
+            exit 0
+            ;;
+    esac
+done
+
+clrecho() {
+    printf "\e[38;5;196m$1\e[0m\n"
+}
+
+###################################################################
+
+
+if [[ "$OPT_APP_FORMAT" == "app" ]]; then
     set -x
     sed -Ei '' "s/version: (.*)\+([0-9]+$)/version: \1\+$BUILD_NUMBER/g" pubspec.yaml
-    sed -Ei '' "s/static const int COPYRIGHT_DATE = ([0-9]+);$/static const int COPYRIGHT_DATE = `date "+%Y"`;/g" lib/settings.dart
+    sed -Ei '' "s/static const int COPYRIGHT_DATE = ([0-9]+);$/static const int COPYRIGHT_DATE = $YEAR_NUMBER;/g" lib/settings.dart
+
     fvm flutter build appbundle --obfuscate --release --split-debug-info=debug_info/$BUILD_NUMBER
+    BUILD_RESULT=$?
+
     set +x
     # Update the build number
     echo $BUILD_NUMBER > .build_number
-elif [[ "$1" == "apk" ]]; then
-    set -x
+elif [[ "$OPT_APP_FORMAT" == "apk" ]]; then
     fvm flutter build apk --obfuscate --release --split-per-abi --split-debug-info=debug_info/$BUILD_NUMBER --build-number $BUILD_NUMBER
-    set +x
-    # Update the build number
-    echo $BUILD_NUMBER > .build_number
+    BUILD_RESULT=$?
+
+    if [[ $BUILD_RESULT == 0 ]]; then
+        # Update the build number
+        echo $BUILD_NUMBER > .build_number
+    else
+        clrecho "Failed to build the $OPT_APP_FORMAT"
+        return;
+    fi
 fi
 
-if [[ "$1" == "--deploy" || "$1" == "-d" || "$2" == "--deploy" || "$2" == "-d" ]]; then
+if [[ $BUILD_RESULT == 0 && $OPT_DEPLOYMENT == "-d" ]]; then
     scp2p30p build/app/outputs/apk/release/app-arm64-v8a-release.apk /root/download/
 fi
+
+if [[ $BUILD_RESULT == 0 && $OPT_UPLOAD == "-u" ]]; then
+    local VERSION_NUM=`awk '/version:/ {gsub(/.*: /,"",$0); gsub(/\+.*/,"",$0); print $0}' pubspec.yaml`
+    scp2da build/app/outputs/apk/release/app-arm64-v8a-release.apk "~/dashare/download/dashare-v${VERSION_NUM}.apk"
+fi
+
